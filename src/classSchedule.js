aa078e261ba188f14f1f40a3dcdee442edbaa877
insidepixce <insidepixce@gmail.com>
2023-12-18 11:27:20 +0900

    뼈대 완성했으나 이제 더 꾸며야 함

---
+++ src/classSchedule.js
@@ -0,0 +1,18 @@
+// 성빈이와 교담이의 시간표
+const classSchedule = [
+    { time: '7:30', text: '아침식사, 세면' },
+    { time: '8:00', text: '1교시' },
+    { time: '9:00', text: '2교시' },
+    { time: '10:00', text: '3교시' },
+    { time: '11:00', text: '4교시' },
+    { time: '12:00', text: '점심시간' },
+    { time: '13:10', text: '5교시' },
+    { time: '14:10', text: '6교시' },
+    { time: '15:10', text: '7교시' },
+    { time: '16:10', text: '8교시' },
+    { time: '17:10', text: '9교시' },
+    { time: '18:00', text: '저녁식사' },
+  ];
+  
+  module.exports = classSchedule;
+  
