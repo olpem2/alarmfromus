12a9fbf0c3d73937ac337297ba88b6cbcc3479bf
insidepixce <insidepixce@gmail.com>
2023-12-18 15:31:16 +0900

    페이지 추가

--- src/classSchedule.js
+++ src/classSchedule.js
@@ -6,11 +6,11 @@ const classSchedule = [
     { time: '10:00', text: '3교시' },
     { time: '11:00', text: '4교시' },
     { time: '12:00', text: '점심시간' },
-    { time: '13:10', text: '5교시' },
-    { time: '14:10', text: '6교시' },
-    { time: '15:10', text: '7교시' },
-    { time: '16:10', text: '8교시' },
-    { time: '17:10', text: '9교시' },
+    { time: '13:00', text: '5교시' },
+    { time: '14:00', text: '6교시' },
+    { time: '15:00', text: '7교시' },
+    { time: '16:00', text: '8교시' },
+    { time: '17:00', text: '9교시' },
     { time: '18:00', text: '저녁식사' },
   ];
   
